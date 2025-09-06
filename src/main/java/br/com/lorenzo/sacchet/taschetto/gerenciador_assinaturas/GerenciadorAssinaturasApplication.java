2011831954000f337dce2738e8e3b54b073720ee
Lorenzo Taschetto <lorenzotaschetto04@gmail.com>
2025-09-06 18:17:43 -0300

    chore: initial project setup

---
+++ src/main/java/br/com/lorenzo/sacchet/taschetto/gerenciador_assinaturas/GerenciadorAssinaturasApplication.java
@@ -0,0 +1,13 @@
+package br.com.lorenzo.sacchet.taschetto.gerenciador_assinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorAssinaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorAssinaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lorenzo/sacchet/taschetto/gerenciador_assinaturas/GerenciadorAssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lorenzo.sacchet.taschetto.gerenciador_assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorAssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
